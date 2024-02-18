03ae7d9833f5cfb63172c4f5b975fedb347421ac
LGZX <13522110321@139.com>
2024-02-18 11:51:41 +0800

    Depth First Search Function Completion Code

--- code.cpp
+++ code.cpp
@@ -20,8 +20,21 @@ char maze[18][24]= {"************** ********",//1 The default setting is' * 'as 
 				 	"*************** *******",//17 
 				 	"***********************"
 				 	};//18
+int vis[18][24],dx[4]={0,1,0,-1},dy[4]={1,0,-1,0},flag=0,ans=0,anses=0;
 void dfs(int x,int y,int ans_){
-    
+    if(maze[x][y]=='$'){
+        flag=1;
+        anses++;
+        ans=ans_;
+        return ;
+    }
+    for(int i=0;i<4;i++){
+        if(maze[x+dx[i]][y+dy[i]]!='*'&&vis[x+dx[i]][y+dy[i]]==0){
+            vis[x][y]=1;
+            dfs(x+dx[i],y+dy[i],ans_+1);
+            vis[x][y]=0;
+        }
+    }
 }
 int main(void){
     
